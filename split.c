36621f5bf0915f46c692f7a29c4a3402c43bbab5
felix igwe <felixigwe55@gmail.com>
2022-11-16 13:58:06 -0800

    upsated simple shell

--- split.c
+++ split.c
@@ -4,7 +4,7 @@
  * swap_char - swaps | and & for non-printed chars
  *
  * @input: inputstring
- * @bool: type of swap
+ * @bool: type of swap.
  * Return: swapped string
  */
 char *swap_char(char *input, int bool)
