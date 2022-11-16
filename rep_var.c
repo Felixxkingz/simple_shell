3688b9b1be01c6d61ed448733d9afdac5d98d792
Bossify <111274844+Bossify@users.noreply.github.com>
2022-11-16 12:54:26 -0800

    done

--- rep_var.c
+++ rep_var.c
@@ -4,7 +4,7 @@
  * check_env - checks if the typed variable is an env variable
  *
  * @h: head of linked list
- * @in: input string
+ * @in: input strinng
  * @data: data structure
  * Return: no return
  */
